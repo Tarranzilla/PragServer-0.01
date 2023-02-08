276e0a2f92b97b7d6e37aaa764d4d9154de2da72
JOAO TARRAN <joaotarran@gmail.com>
2023-02-08 01:58:26 -0300

    Update (Buggy)

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+//app.use(cors());
 //app.use(getWeather);
 
 app.set("view engine", "ejs");
